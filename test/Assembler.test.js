b1b625411333d981cd324f19b171c3c2dfcd6892
Arvin Lin <a7lin@ucsd.edu>
2020-06-24 16:47:42 -0700

    Add unit test for validateTypeTwoInstruction

--- test/Assembler.test.js
+++ test/Assembler.test.js
@@ -1,7 +1,7 @@
 import Assembler from '../src/js/Assembler';
 
 describe('Assembler Simple Test', () => {
-    let assembler = new Assembler([]);
+    let assembler = new Assembler(['loop:','SMALLER:']);
 
     it('Assember getInstructionName Simple Test', () => {
         
@@ -59,6 +59,11 @@ describe('Assembler Simple Test', () => {
 
     });
 
+    it('Assembler looup Simple Test', () => {
+
+        expect(assembler.lookup('loop')).toBeGreaterThan(-1);
+    })
+
     it('Assembler validateInstructionName Simple Test', async () => {
 
         let result = await assembler.validateInstructionName('mov r1, r2');
@@ -70,5 +75,70 @@ describe('Assembler Simple Test', () => {
         } catch (error) {
             expect(error).toBeInstanceOf(Error);
         }
-    })
+    });
+
+    it('Assembler validateTypeZeroInstruction Simple Test', async () => {
+
+        let result = await assembler.validateTypeZeroInstruction('ble loop');
+
+        expect(result).toBe('loop');
+
+        result = await assembler.validateTypeZeroInstruction('b SMALLER');
+
+        expect(result).toBe('SMALLER');
+
+        try {
+            await assembler.validateTypeZeroInstruction('b LABEL3');
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+        }
+
+        try {
+            await assembler.validateTypeZeroInstruction('b label, label2');
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+        }
+    });
+
+    it('Assembler validateTypeOneInstruction Simple Test', async () => {
+
+        let result = await assembler.validateTypeOneInstruction('eor r1, r5, r9');
+
+        expect(result).toEqual(['r1', 'r5', 'r9']);
+
+        result = await assembler.validateTypeOneInstruction('sub r1, r5, #100');
+
+        expect(result).toEqual(['r1', 'r5', '#100']);
+
+        try {
+            await assembler.validateTypeOneInstruction('eor r1, r5, 66');
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+        }
+
+        try {
+            await assembler.validateTypeOneInstruction('eor r1, r5');
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+        }
+    });
+
+    it('Assembler validateTypeTwoInstruction Simple Test', async () => {
+        
+        let result = await assembler.validateTypeTwoInstruction('ldr r1, [r2]');
+
+        expect(result).toEqual(['r1', '[r2]']);
+
+        result = await assembler.validateTypeTwoInstruction('ldr r1, [r2], #4');
+
+        expect(result).toEqual(['r1', '[r2]', '#4']);
+
+        result = await assembler.validateTypeTwoInstruction('ldr r1, [r2, #10]');
+
+        expect(result).toEqual(['r1', '[r2', '#10]']);
+
+        result = await assembler.validateTypeTwoInstruction('ldr r1, [r2, r3]');
+
+        expect(result).toEqual(['r1', '[r2', 'r3]']);
+    });
 })
