a6665a9fb97127c050dc97de5e95d1f527048390
Arvin Lin <a7lin@ucsd.edu>
2021-06-11 18:50:32 +0800

    Add Decoder sanity test

---
+++ src/sim/decoder/decoder.test.ts
@@ -0,0 +1,100 @@
+import Decoder from './decoder';
+import RegisterFile from '../register/register';
+
+describe('Decoder Sanity Test', () => {
+    it('Dummy Assembled Instruction Test', () => {
+        let ambInstr: string[][] = [
+            ['mov', 'r1', '1'],
+            ['sub', 'r1', 'r2', '3'],
+            ['ldr', 'r1', '[r2]'],
+            ['cmp', 'r1', 'r2'],
+            ['bgt', 'name'],
+            ['lsl', 'r1', 'r2']
+        ];
+        let regFile: RegisterFile = new RegisterFile()
+        let decoder: Decoder = new Decoder(ambInstr, regFile);
+
+        expect(decoder.hasNext()).toBe(true);
+        expect(decoder.currInstrName).toStrictEqual('mov');
+        expect(decoder.currInstrType).toBe(4);
+        expect(decoder.control).toStrictEqual({
+            writeBack: true,
+            memRead: false,
+            memWrite: false,
+            jump: false,
+            preIndex: false,
+            postIndex: false
+        });
+
+        /* Process current instruction */
+        decoder.next();
+
+        expect(decoder.hasNext()).toBe(true);
+        expect(decoder.currInstrName).toStrictEqual('sub');
+        expect(decoder.currInstrType).toBe(1);
+        expect(decoder.control).toStrictEqual({
+            writeBack: true,
+            memRead: false,
+            memWrite: false,
+            jump: false,
+            preIndex: false,
+            postIndex: false
+        });
+
+        decoder.next();
+
+        expect(decoder.hasNext()).toBe(true);
+        expect(decoder.currInstrName).toStrictEqual('ldr');
+        expect(decoder.currInstrType).toBe(2);
+        expect(decoder.control).toStrictEqual({
+            writeBack: true,
+            memRead: true,
+            memWrite: false,
+            jump: false,
+            preIndex: false,
+            postIndex: false
+        });
+
+        decoder.next();
+
+        expect(decoder.hasNext()).toBe(true);
+        expect(decoder.currInstrName).toStrictEqual('cmp');
+        expect(decoder.currInstrType).toBe(4);
+        expect(decoder.control).toStrictEqual({
+            writeBack: false,
+            memRead: false,
+            memWrite: false,
+            jump: false,
+            preIndex: false,
+            postIndex: false
+        });
+
+        decoder.next();
+
+        expect(decoder.hasNext()).toBe(true);
+        expect(decoder.currInstrName).toStrictEqual('bgt');
+        expect(decoder.currInstrType).toBe(0);
+        expect(decoder.control).toStrictEqual({
+            writeBack: false,
+            memRead: false,
+            memWrite: false,
+            jump: true,
+            preIndex: false,
+            postIndex: false
+        });
+
+        decoder.next();
+
+        expect(decoder.hasNext()).toBe(false);
+        expect(decoder.currInstrName).toStrictEqual('lsl');
+        expect(decoder.currInstrType).toBe(1);
+        expect(decoder.control).toStrictEqual({
+            writeBack: true,
+            memRead: false,
+            memWrite: false,
+            jump: false,
+            preIndex: false,
+            postIndex: false
+        });
+    });
+});
--- src/sim/decoder/decoder.ts
+++ src/sim/decoder/decoder.ts
@@ -36,8 +36,9 @@ export default class Decoder {
             postIndex: false
         };
         this.instr = assembledInstr;
-        this.currInstr = null as any;
+        this.currInstr = this.instr[regFile.block.pc];
         this.regFile = regFile;
+        this.updateControl();
     }
 
     get currInstrName(): string {
@@ -59,42 +60,50 @@ export default class Decoder {
         };
     }
 
+    updateControl() {
+        let currType: number = this.currInstrType;
+
+        if (currType == 0) {
+            this.control.writeBack = false;
+            this.control.jump = true;
+        }
+
+        if (currType == 2) {
+            if (this.currInstrName.includes('str')) {
+                this.control.writeBack = false;
+                this.control.memWrite = true;
+            } else {
+                this.control.memRead = true;
+            }
+
+            /* Check the argument for pre-index and post-index */
+            let last: string = this.currInstr[this.currInstr.length - 1];
+            if (last.charAt(last.length - 1) === '!' &&
+                this.currInstr.length >= 4) {
+                this.control.preIndex = true;
+            } else if (last.charAt(last.length - 1) !== ']' &&
+                this.currInstr.length >= 4) {
+                this.control.postIndex = true;
+            }
+        }
+
+        if (currType == 4) {
+            if (this.currInstrName === 'cmp') {
+                this.control.writeBack = false;
+            }
+        }
+    }
+
     hasNext(): boolean {
-        return this.instr[this.regFile.block.pc] !== undefined;
+        return this.instr[this.regFile.block.pc + 1] !== undefined;
     }
 
     next() {
         if (this.hasNext()) {
-            this.currInstr = this.instr[this.regFile.block.pc];
             this.regFile.block.pc++;
+            this.currInstr = this.instr[this.regFile.block.pc];
             this.resetControl();
-
-            /* Update the control signal based on the instruction type */
-            let currType: number = this.currInstrType;
-
-            if (currType == 0) {
-                this.control.writeBack = false;
-                this.control.jump = true;
-            }
-
-            if (currType == 2) {
-                if (this.currInstrName.includes('str')) {
-                    this.control.writeBack = false;
-                    this.control.memWrite = true;
-                } else {
-                    this.control.memRead = true;
-                }
-
-                /* Check the argument for pre-index and post-index */
-                let last: string = this.currInstr[this.currInstr.length - 1];
-                if (last.charAt(last.length - 1) === '!' &&
-                    this.currInstr.length >= 4) {
-                    this.control.preIndex = true;
-                } else if (last.charAt(last.length - 1) !== ']' &&
-                    this.currInstr.length >= 4) {
-                    this.control.postIndex = true;
-                }
-            }
+            this.updateControl();
         }
     }
 }
