1c40c7b6c3c4e9783a4098b142f9f027b07ed950
Arvin Lin <a7lin@ucsd.edu>
2021-06-16 10:08:59 +0800

    Change tab to space of 2

--- src/var/action/action.ts
+++ src/var/action/action.ts
@@ -2,33 +2,33 @@ import Simulation from "../../sim/simulation";
 import Editor from "../editor/editor";
 
 export default class Action {
-    /**
-     * Action container
-     */
+  /**
+   * Action container
+   */
 
-    sim: Simulation;
-    editor: Editor;
+  sim: Simulation;
+  editor: Editor;
 
-    constructor(inSim: Simulation, inEditor: Editor) {
-        this.sim = inSim;
-        this.editor = inEditor;
+  constructor(inSim: Simulation, inEditor: Editor) {
+    this.sim = inSim;
+    this.editor = inEditor;
 
-        this.setup = this.setup.bind(this);
+    this.setup = this.setup.bind(this);
 
-        this.setup();
-    }
+    this.setup();
+  }
 
-    setup() {
-        let stepButton = document.getElementById('step-btn');
-        let buildButton = document.getElementById('build-btn');
-        let runButton = document.getElementById('run-btn');
-    
-        buildButton?.addEventListener('click', () => {
-          this.sim.assemble(this.editor.code);
-        });
-    
-        stepButton?.addEventListener('click', () => {
-          this.sim.step();
-        })
-      }
+  setup() {
+    let stepButton = document.getElementById('step-btn');
+    let buildButton = document.getElementById('build-btn');
+    let runButton = document.getElementById('run-btn');
+  
+    buildButton?.addEventListener('click', () => {
+      this.sim.assemble(this.editor.code);
+    });
+
+    stepButton?.addEventListener('click', () => {
+      this.sim.step();
+    })
+  }
 }
